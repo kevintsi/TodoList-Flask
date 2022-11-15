3c9cbe0cefe3c40c0cbe6bcdb526fdec021659f7
Kévin TSI <kevtsiii@gmail.com>
2022-11-15 15:18:18 +0100

    Route de suppression locale finie

--- front/main.py
+++ front/main.py
@@ -11,17 +11,23 @@ todos = [
 
 @app.get("/")
 def index():
-
+    print(todos)
     return render_template("todo_list/todo_list.htm", todos=todos)
 
 
 @app.post("/add")
 def add_task():
-    print(request.form["task"])
-    todos.append({"id": 4, "task": request.form["task"]})
+    global todos
+    print(request.form["new_task"])
+    todos.append({"id": '4', "task": request.form["new_task"]})
     return redirect(url_for("index"))
 
 
-@app.delete("/delete")
+@app.post("/delete")
 def delete_task():
-    pass
+    global todos
+    ids = request.form.getlist("task")
+    print(ids)
+    todos = list(filter(
+        lambda item: True if not item["id"] in ids else False, todos))
+    return redirect(url_for("index"))
